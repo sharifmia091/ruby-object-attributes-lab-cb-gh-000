64f8ef33745e0590d799335af0e026d7ce16920b
md mia <sharifmia091@gmail.com>
2018-08-28 05:17:58 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,13 @@
 class Dog
 
-  def name(dogs_name)
-    name = dogs_name
+  def name=(dogs_name)
+    this_dogs_name = dogs_name
   end
+
+  def name
+    this_dogs_name
+  end
+  
 end
 
 fido = Dog.new
